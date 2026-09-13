7b7e12eeec7ed4da9c1cd0c63e980b2085ebf7d6
MexrlDev <82407748+MexrlDev@users.noreply.github.com>
2026-09-13 22:15:38 +0300

    Update version.h

--- installer/include/version.h
+++ installer/include/version.h
@@ -1,8 +1,9 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSION "2.2.0 BETA"
-#define MIN_FW 474
-#define MAX_FW 1352
+#define HEN_NAME "PS4HEN"
+#define VERSION  "2.2.1 BETA"
+#define MIN_FW   474
+#define MAX_FW   1352
 
 #endif
