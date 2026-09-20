5af04f1381a410bfa6bf5997424c50a2484eab10
MexrlDev <82407748+MexrlDev@users.noreply.github.com>
2026-09-20 03:32:12 +0300

    Update config.h

--- installer/include/config.h
+++ installer/include/config.h
@@ -1,13 +1,21 @@
-#ifndef CONFIG_H_
-#define CONFIG_H_
+#ifndef CONFIG_STRUCT_H_
+#define CONFIG_STRUCT_H_
 
-#include "config_struct.h"
+#define TARGET_ID_SIZE 4 // eg. 0x84
 
-#define DEFAULT_CONFIG_VERSION 3
-
-void upload_ini(const char *path);
-int init_config(struct configuration *config);
-
-extern int found_version;
+struct configuration {
+  int config_version;
+  int exploit_fixes;
+  int mmap_patches;
+  int block_updates;
+  int disable_aslr;
+  int nobd_patches;
+  int skip_patches;
+  int upload_prx;
+  int enable_plugins;
+  int show_network_info; // NEW: 0 = hide IP/klog notify, 1 = show
+  int show_title_id;     // NEW: 0 = hide title IDs in PKG listings, 1 = show
+  char target_id[TARGET_ID_SIZE + 1];
+};
 
 #endif
